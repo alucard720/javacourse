1434b43c17c94581eaa9580564e75cb405cfc638
alucard720 <ing.santana36@gmail.com>
2023-04-10 12:51:59 -0400

    2nd commit mac

---
+++ practica de condicionales/bucle.js
@@ -0,0 +1,21 @@
+/* while(condicion){
+    bloque de codigo
+} */
+
+/*
+do{
+    bloque de codigo
+}while(condicion);
+ */
+
+/* 
+for(inicializacion; condicion; incremento){
+    bloque de codigo
+}
+ej. for (let i=0; i < 10; i++){
+    console.log(i);
+}
+==for arrays==
+for (let i = 0; i < values.length; i++) {
+    sum += values[i];
+*/
